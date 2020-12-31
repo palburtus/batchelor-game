c445f97945dfc7df8955794742aa6c14fca5ed81
Patrick Alburtus <palburtus@webmd.net>
2020-12-30 19:15:07 -0500

    added dedupping, final two final one, and moved game specific state into single object

--- src/batchelorGame.jsx
+++ src/batchelorGame.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Container, ListGroup, ListGroupItem, Col, Row, Card } from 'react-bootstrap';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { ToastContainer, toast } from 'react-toastify';
 import * as constants from './constants'
+import 'react-toastify/dist/ReactToastify.css';
 
 class BatchelorGame extends React.Component {
     
     constructor(props){
         super(props);
-
+        let picks = {
+            finalFour: [],
+            finalTwo: [],
+            finalOne: -1
+        }
         this.state = ({
-            finalFour: []
+            picks: picks
         });
 
         this.onDragEnd = this.onDragEnd.bind(this);
@@ -21,30 +27,76 @@ class BatchelorGame extends React.Component {
 
     onDragEnd(result) {
         const { source, destination } = result;
+        
+        let picks = this.state.picks;
 
-        if(destination.droppableId === 'final-four'){
-            let finalFour = this.state.finalFour;
-            finalFour.push(constants.girls[result.draggableId - 1]);
-            
-            this.setState({
-                finalFour: finalFour
-            });
+        if(destination && destination.droppableId === 'final-four'){
+        
+            if(this.state.picks.finalFour.length >= 4){
+                toast("You must remove a contestant first! (4 Max)", { type: toast.TYPE.ERROR, hideProgressBar: true, autoClose: 2500});
+            }else if(this.state.picks.finalFour.includes(result.draggableId)){
+                toast("You have already added this contestant", { type: toast.TYPE.WARNING, hideProgressBar: true, autoClose: 2500});
+            }else {
+                picks.finalFour = this.state.picks.finalFour;
+                
+                picks.finalFour.push(result.draggableId);
+            }
         }
+
+        if(destination && destination.droppableId === 'final-two'){
+            if(this.state.picks.finalTwo.length >= 2){
+                toast("You must remove a contestant first! (3 Max)", { type: toast.TYPE.ERROR, hideProgressBar: true, autoClose: 2500});
+            }else if(this.state.picks.finalTwo.includes(result.draggableId)){
+                toast("You have already added this contestant", { type: toast.TYPE.WARNING, hideProgressBar: true, autoClose: 2500});
+            }else {
+                picks.finalTwo = this.state.picks.finalTwo;
+                
+                picks.finalTwo.push(result.draggableId);                
+            }
+        }
+
+        if(destination && destination.droppableId === 'final-one'){
+            if(this.state.picks.finalOne >= 1){
+                toast("You must remove a contestant first! (1 Max)", { type: toast.TYPE.ERROR, hideProgressBar: true, autoClose: 2500});
+            }else {
+                picks.finalOne = this.state.picks.finalOne;
+                
+                picks.finalOne = result.draggableId;  
+            }
+        }
+        
+        this.setState({
+            picks: picks
+        });
 
     }
 
     render(){
 
         let finalFourDisplay = 'no selections made';
-        if(this.state.finalFour.length > 0){
-            finalFourDisplay = this.state.finalFour.map((e) => 
-                <li className="names-list-item">{e.name}</li>)
+        
+        if(this.state.picks.finalFour.length > 0){
+            finalFourDisplay = this.state.picks.finalFour.map((id) => 
+                <li className="names-list-item">{constants.girls[id - 1].name}</li>)
         }
-                                                    
+
+        let finalTwoDisplay = 'no selections made';
+        
+        if(this.state.picks.finalTwo.length > 0){
+            finalTwoDisplay = this.state.picks.finalTwo.map((id) => 
+                <li className="names-list-item">{constants.girls[id - 1].name}</li>)
+        }
+
+        let finalOneDisplay = 'No One Gets Final Rose';
+
+        if(this.state.picks.finalOne > 0){
+            finalOneDisplay = <li className="names-list-item">{constants.girls[this.state.picks.finalOne - 1].name}</li>
+        }
 
         return(
             <div>
                 <Container>
+                    <ToastContainer />
                     <DragDropContext onDragEnd={this.onDragEnd}>
                         <Row>
                             <Col>
@@ -95,6 +147,46 @@ class BatchelorGame extends React.Component {
                                                             ref={provided.innerRef}
                                                             isDraggingOver={snapshot.isDraggingOver} >
                                                             {finalFourDisplay}
+                                                        </ul>
+                                                    )
+                                                }
+                                            </Droppable>
+                                    
+                                    </Card.Body>                                
+                                </Card>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>Final Two</Card.Title>
+                                        <Card.Subtitle>Drag 2 Selection (order doesn't matter)</Card.Subtitle>
+                                        
+                                            <Droppable droppableId="final-two">
+                                                {                                                    
+                                                    (provided, snapshot) => (
+                                                        <ul className="names-list"  
+                                                            {...provided.droppableProps} 
+                                                            ref={provided.innerRef}
+                                                            isDraggingOver={snapshot.isDraggingOver} >
+                                                            {finalTwoDisplay}
+                                                        </ul>
+                                                    )
+                                                }
+                                            </Droppable>
+                                    
+                                    </Card.Body>                                
+                                </Card>
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>Final Rose</Card.Title>
+                                        <Card.Subtitle>Drag 1 Selection or don't leave a name if you think no one gets the final rose</Card.Subtitle>
+                                        
+                                            <Droppable droppableId="final-one">
+                                                {                                                    
+                                                    (provided, snapshot) => (
+                                                        <ul className="names-list"  
+                                                            {...provided.droppableProps} 
+                                                            ref={provided.innerRef}
+                                                            isDraggingOver={snapshot.isDraggingOver} >
+                                                            {finalOneDisplay}
                                                         </ul>
                                                     )
                                                 }
